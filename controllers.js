f06b91181c04a60dd8fac20eecbe5429e6a5bb8f
Darrow Cole <darrowco@library.ucla.edu>
2016-04-18 10:35:07 -0700

    first commit

---
+++ controllers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* Controllers */
+
+var myAppModule = angular.module('myAppModule', []);
+myAppModule.controller('myAppController', function($scope,calculateService,$http,$window) {
+  $scope.quantity=1;
+  $scope.quantityResult = 0;
+  $scope.myWelcome = 0;
+  
+  $scope.calculateQuantity = function() {
+
+   $scope.quantityResult = calculateService.calculate($scope.quantity, 10, $scope.myWelcome);
+  };
+});
+
+// Service 
+myAppModule.factory('calculateService', function($window,$http){
+  return {
+    calculate: function(xval,yval,ip){
+      $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source'
+    }  
+  };             
+});
+
