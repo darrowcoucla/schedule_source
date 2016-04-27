ebfd77d263da7c985515f38878834e74e002df2a
Darrow Cole <darrowco@library.ucla.edu>
2016-04-27 16:34:59 -0700

    set to allow auto-redirect, 1button or 8buttons

--- controllers.js
+++ controllers.js
@@ -3,23 +3,32 @@
 /* Controllers */
 
 var myAppModule = angular.module('myAppModule', []);
-myAppModule.controller('myAppController', function($scope,calculateService,$http,$window) {
+myAppModule.controller('myAppController', function($scope,$document,calculateService,$http,$window,$interpolate) {
   $scope.quantity=1;
   $scope.quantityResult = 0;
   $scope.myWelcome = 0;
-  
-  $scope.calculateQuantity = function() {
 
-   $scope.quantityResult = calculateService.calculate($scope.quantity, 10, $scope.myWelcome);
+  if( $window.option == "auto" ){
+    $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source?work_loc='+$window.auto_work_loc;
+  }
+
+  $scope.calculateQuantity = function(work_loc) {
+  $scope.work_loc = work_loc;
+
+
+ alert(work_loc);
+
+   $scope.quantityResult = calculateService.calculate($scope.work_loc);
   };
 });
 
 // Service 
 myAppModule.factory('calculateService', function($window,$http){
   return {
-    calculate: function(xval,yval,ip){
+    calculate: function(work_loc){
 //      $window.location.href='https://www-test.library.ucla.edu/schedule_source'
-      $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source'
+
+      $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source?work_loc='+work_loc
     }  
   };             
 });
