74993849f1c514026ed252da03003616eabeb2de
darrowco <darrowco@ubuntu.home.lan>
2016-05-05 16:23:29 -0700

    updated naming scheme

--- controllers.js
+++ controllers.js
@@ -2,8 +2,8 @@
 
 /* Controllers */
 
-var myAppModule = angular.module('myAppModule', []);
-myAppModule.controller('myAppController', function($scope,$document,calculateService,$http,$window,$interpolate) {
+var scheduleAppModule = angular.module('scheduleAppModule', []);
+scheduleAppModule.controller('scheduleAppController', function($scope,$document,moduleService,$http,$window,$interpolate) {
   $scope.quantity=1;
   $scope.quantityResult = 0;
   $scope.myWelcome = 0;
@@ -12,19 +12,16 @@ myAppModule.controller('myAppController', function($scope,$document,calculateSer
     $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source?work_loc='+$window.auto_work_loc;
   }
 
-  $scope.calculateQuantity = function(work_loc) {
+  $scope.deptUsed = function(work_loc) {
   $scope.work_loc = work_loc;
-
-  // alert(work_loc);
-
-   $scope.quantityResult = calculateService.calculate($scope.work_loc);
+   $scope.quantityResult = moduleService.redirect($scope.work_loc);
   };
 });
 
 // Service 
-myAppModule.factory('calculateService', function($window,$http){
+scheduleAppModule.factory('moduleService', function($window,$http){
   return {
-    calculate: function(work_loc){
+    redirect: function(work_loc){
       $window.location.href='https://www-test.library.ucla.edu/schedule_source?work_loc='+work_loc
 //      $window.location.href='http://uclalib.local/UCLALIB/www/schedule_source?work_loc='+work_loc
     }  
